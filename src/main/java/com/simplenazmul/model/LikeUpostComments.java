c556795b6404f656e597907810f1fe22ad529dd4
SimpleNazmul <simplenazmul@gmail.com>
2016-12-03 16:28:16 +0600

    Updates

--- src/main/java/com/simplenazmul/model/LikeUpostComments.java
+++ src/main/java/com/simplenazmul/model/LikeUpostComments.java
@@ -29,4 +29,79 @@ public class LikeUpostComments {
 
 	@Column(name = "date_liked", nullable = false)
 	private Timestamp dateLiked;
+
+	public int getLikeId() {
+		return likeId;
+	}
+
+	public void setLikeId(int likeId) {
+		this.likeId = likeId;
+	}
+
+	public int getUpostCommentsId() {
+		return upostCommentsId;
+	}
+
+	public void setUpostCommentsId(int upostCommentsId) {
+		this.upostCommentsId = upostCommentsId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getLikeValue() {
+		return likeValue;
+	}
+
+	public void setLikeValue(int likeValue) {
+		this.likeValue = likeValue;
+	}
+
+	public Timestamp getDateLiked() {
+		return dateLiked;
+	}
+
+	public void setDateLiked(Timestamp dateLiked) {
+		this.dateLiked = dateLiked;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dateLiked == null) ? 0 : dateLiked.hashCode());
+		result = prime * result + upostCommentsId;
+		result = prime * result + userId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LikeUpostComments other = (LikeUpostComments) obj;
+		if (dateLiked == null) {
+			if (other.dateLiked != null)
+				return false;
+		} else if (!dateLiked.equals(other.dateLiked))
+			return false;
+		if (upostCommentsId != other.upostCommentsId)
+			return false;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}
+
+
+	
+	
 }
